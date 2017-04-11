7aa0eb4569c433dde64345aca74c38d3eac074b1
ssharma <ssharma9605@gmail.com>
2017-04-11 17:43:08 -0400

    This is sample push

--- app/src/main/java/com/example/trustbuy/trustbuy_ss/LoginActivity.java
+++ app/src/main/java/com/example/trustbuy/trustbuy_ss/LoginActivity.java
@@ -9,7 +9,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-
+//this is java file
 public class LoginActivity extends UserAccountsettingsActivity {
 
 
